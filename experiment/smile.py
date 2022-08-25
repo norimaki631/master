6529d02ee9b3e323982bdb5aadc86f102ab95df7
norimaki631 <10kiwi05310@gmail.com>
2022-08-25 21:14:38 +0900

    1調整

--- experiment/smile.py
+++ experiment/smile.py
@@ -47,7 +47,7 @@ while True:
     # [TO DO?]ウィンドウサイズを抽出して絶対値で指定する ※上のタブも入らないように！！！
     # この数値はpcAにおいてピン止めして被験者を右に寄せたウィンドウを右のモニターの右上端に寄せる
     # (Rectangle.left, Rectangle.top, Rectangle.right, Rectangle.bottom)
-    original = SCT((980, 135, 1600, 450))
+    original = SCT((980, 135, 1600, 490))
     original = cv2.resize(original, (int(original.shape[1]*0.7), int(original.shape[0]*0.7)))
     image = torch.from_numpy(original).to(device)
 
